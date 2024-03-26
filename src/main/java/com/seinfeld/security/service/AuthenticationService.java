74a17c7e65252e055861ba8abf698a6b8382cdd0
Ali <qali372@gmail.com>
2024-03-26 22:21:38 +0200

    build : AuthenticationService.java

---
+++ src/main/java/com/seinfeld/security/service/AuthenticationService.java
@@ -0,0 +1,57 @@
+package com.seinfeld.security.service;
+
+import com.seinfeld.security.config.JwtService;
+import com.seinfeld.security.model.Role;
+import com.seinfeld.security.model.User;
+import com.seinfeld.security.repository.UserRepository;
+import com.seinfeld.security.request.LoginRequest;
+import com.seinfeld.security.request.RegisterRequest;
+import com.seinfeld.security.response.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
