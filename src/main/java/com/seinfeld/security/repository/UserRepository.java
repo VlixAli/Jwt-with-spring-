3eb31b46eb6e10b6a65a229b43c7fb6371f70032
Ali <qali372@gmail.com>
2024-03-26 22:19:49 +0200

    build : UserRepository.java

---
+++ src/main/java/com/seinfeld/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.seinfeld.security.repository;
+
+import com.seinfeld.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
